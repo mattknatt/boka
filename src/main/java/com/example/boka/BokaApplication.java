0c7ca1c61f79580436bbfc0a3f0cbad38575bbe4
mattknatt <mattiashagstrommusic@gmail.com>
2026-02-12 08:47:55 +0100

    Initial commit: Spring Boot 4.0.2 project setup with PostgreSql

---
+++ src/main/java/com/example/boka/BokaApplication.java
@@ -0,0 +1,13 @@
+package com.example.boka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BokaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BokaApplication.class, args);
+	}
+
+}
