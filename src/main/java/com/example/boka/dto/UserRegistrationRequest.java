b5c1b45b41be25e4ff20b9a4e726e8f2dce0367a
mattknatt <mattiashagstrommusic@gmail.com>
2026-04-12 08:54:26 +0200

    password validation requirements

--- src/main/java/com/example/boka/dto/UserRegistrationRequest.java
+++ src/main/java/com/example/boka/dto/UserRegistrationRequest.java
@@ -17,5 +17,5 @@ public class UserRegistrationRequest {
 
     @NotBlank @Email private String email;
 
-    @NotBlank @Size(min = 6) private String password;
+    @NotBlank @Size(min = 8, max = 100) private String password;
 }
